f650a9581faae5e26a3fe51d38e58db6708de9c2
Sagar Ippalpalli <i.vikram15@gmail.com>
2016-04-20 20:37:50 +0530

    updated

--- BE-cl3/wednesday/8 queen/test_queens.py
+++
@@ -1,9 +0,0 @@
-from eight_queens import solution
-import unittest
-
-class TestEightQueens(unittest.TestCase):
-	def test_solution(self):
-		self.assertEqual(solution(),[(4, 1), (7, 2), (5, 3), (2, 4), (6, 5), (1, 6), (3, 7)])
-
-if __name__ == "__main__":
-	unittest.main()
--- BE-cl3/wednesday/booths/test_booth.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from booth import booth
-class TestBooth(unittest.TestCase):
-	def test_positive(self):
-		self.assertEqual(booth(3,4,8,8),10)
-
-if __name__ == "__main__":
-	unittest.main()
